2699c96013247d73cc9800cb1ddc2c5765d7cb68
Martin Atkins <mart@degeneration.co.uk>
2020-09-12 20:42:15 -0700

    Install required providers and fetch their schemas

--- internal/terrafy/terrafy.go
+++ internal/terrafy/terrafy.go
@@ -6,7 +6,9 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/hashicorp/terraform-exec/tfexec"
@@ -54,13 +56,11 @@ func Run(opts *Options) (map[string]*hcl.File, hcl.Diagnostics) {
 
 	// First we need to get all of the required providers installed, so we can
 	// read their schemas in preparation for our later work.
-	err = generateProviderRequirements(filepath.Join(tmpDir, "versions.tf"), cfg.ProviderReqs, cfg.SourceFiles)
-	if err != nil {
-		diags = diags.Append(&hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  "Failed to create provider requirements file",
-			Detail:   fmt.Sprintf("Could not create a temporary provider requirements file: %s.", err),
-		})
+	moreDiags = generateProviderRequirements(tmpDir, cfg.ProviderReqs, cfg.SourceFiles)
+	diags = append(diags, moreDiags...)
+	if moreDiags.HasErrors() {
+		// If we couldn't generate the requirements file then the rest of
+		// this will not succeed either.
 		return cfg.SourceFiles, diags
 	}
 
@@ -69,16 +69,78 @@ func Run(opts *Options) (map[string]*hcl.File, hcl.Diagnostics) {
 		diags = diags.Append(&hcl.Diagnostic{
 			Severity: hcl.DiagError,
 			Summary:  "Failed to initialize temporary working directory",
-			Detail:   fmt.Sprintf("Could not initialize a temporary working directory to handle the import: %s.", err),
+			Detail:   fmt.Sprintf("Could not initialize a temporary working directory to handle the import:\n\n%s", err),
 		})
 		return cfg.SourceFiles, diags
 	}
 
+	schemas, err := tf.ProvidersSchema(context.Background())
+	if err != nil {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Failed to retrieve provider schemas",
+			Detail:   fmt.Sprintf("Could not retrieve the schemas for the required providers:\n\n%s", err),
+		})
+		return cfg.SourceFiles, diags
+	}
+
+	spew.Dump(schemas)
+
 	return cfg.SourceFiles, diags
 }
 
-func generateProviderRequirements(targetFile string, reqs map[string]hcl.Expression, files map[string]*hcl.File) error {
-	f := hclwrite.NewEmptyFile()
+func generateProviderRequirements(targetDir string, reqs map[string]hcl.Expression, files map[string]*hcl.File) hcl.Diagnostics {
+	// Terraform only installs providers that are actually used by something
+	// in the configuration, so we'll also generate a temporary file
+	// with some placeholder "provider" blocks to force the installation
+	// of each provider. We'll overwrite providers.tf with a real set of
+	// provider configurations in a later step, once we've got all the
+	// providers installed so we can see their schemas.
+	versionsFilename := filepath.Join(targetDir, "versions.tf")
+	providersFilename := filepath.Join(targetDir, "providers.tf")
+	var diags hcl.Diagnostics
+	versionsFile := hclwrite.NewEmptyFile()
+	providersFile := hclwrite.NewEmptyFile()
 
-	return ioutil.WriteFile(targetFile, f.Bytes(), 0700)
+	tfBlock := versionsFile.Body().AppendNewBlock("terraform", nil)
+	reqsBlock := tfBlock.Body().AppendNewBlock("required_providers", nil)
+	names := make([]string, 0, len(reqs))
+
+	for name := range reqs {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		expr := reqs[name]
+		val, moreDiags := expr.Value(nil)
+		diags = append(diags, moreDiags...)
+		if moreDiags.HasErrors() {
+			continue
+		}
+		reqsBlock.Body().SetAttributeValue(name, val)
+
+		providersFile.Body().AppendNewBlock("provider", []string{name})
+	}
+
+	err := ioutil.WriteFile(versionsFilename, versionsFile.Bytes(), 0700)
+	if err != nil {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Failed to create provider requirements file",
+			Detail:   fmt.Sprintf("Could not create a temporary provider requirements file: %s.", err),
+		})
+		return diags
+	}
+
+	err = ioutil.WriteFile(providersFilename, providersFile.Bytes(), 0700)
+	if err != nil {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  "Failed to create provider configurations file",
+			Detail:   fmt.Sprintf("Could not create a temporary provider configurations file: %s.", err),
+		})
+		return diags
+	}
+
+	return diags
 }
